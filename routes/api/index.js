de62e0221b7a9c032a6dc904ae63e4a6bc873ca5
mlemke24 <mattrlemke@gmail.com>
2021-06-06 15:17:09 -0500

    cant believe i missed that

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-
-const pizzaRoutes = require('./pizza-rotues');
 const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-rotues');
 
-router.use('/comments', commentRoutes)
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/pizza-list.html'));
-});
+
 
 router.get('/add-pizza', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/add-pizza.html'));
@@ -11,6 +9,10 @@ router.get('/add-pizza', (req, res) => {
 
 router.get('/pizza', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/pizza.html'));
+});
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/pizza-list.html'));
 });
 
 module.exports = router;
